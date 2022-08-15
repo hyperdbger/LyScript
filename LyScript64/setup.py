ee88e3ead8d1082c29ed7b0e3300ec151a9cc3d6
王瑞 <me@lyshark.com>
2022-08-15 13:28:59 +0800

    Update setup.py

--- LyScript64/setup.py
+++ LyScript64/setup.py
@@ -5,7 +5,7 @@ packages = ['LyScript64']
 
 setup(
     name='LyScript64',
-    version='1.0.11',
+    version='1.0.12',
     author='lyshark',
     description='A powerful x64dbg remote debugging module tools',
     author_email='me@lyshark.com',
