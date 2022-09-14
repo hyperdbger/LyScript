9fff7a5d26916489db1e3a66e1cb7a44296a1086
王瑞 <me@lyshark.com>
2022-09-14 10:38:46 +0800

    Update 通过PEB获取堆基址.py

--- ScriptShare/通过PEB获取堆基址.py
+++ ScriptShare/通过PEB获取堆基址.py
@@ -1,3 +1,8 @@
+# ----------------------------------------------
+# By: LyShark
+# Email: me@lyshark.com
+# Project: https://github.com/lyshark/LyScript
+# ----------------------------------------------
 from LyScript32 import MyDebug
 
 # 获取模块基址
